ba92c9ab319328b95f6830a64c8918889243fac2
AndyHsieh <andy.hsieh@gaiatechs.com>
2023-09-05 18:55:08 +0800

    adjust img load

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -80,6 +80,17 @@ export default function Result() {
         }
     }
 
+    const backgroundImage = new Image();
+    const [isBgImgLoaded, setIsBgImgLoaded] = useState(false)
+    backgroundImage.src = getImageUrl('result-element', '375-bg')
+    backgroundImage.onload = function () {
+        setIsBgImgLoaded(true)
+    };
+
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+
+
     return (
         <div className='px-4 pt-8 pb-36' style={{
             backgroundImage: `url(${getImageUrl('result-element', '375-bg')})`,
@@ -87,10 +98,10 @@ export default function Result() {
             backgroundPosition: "top center",
             backgroundRepeat: "repeat"
         }}>
-            <div className='mx-auto max-w-[400px]'>
-                <img src={imagePath} alt="result-img" className='mx-auto' ref={imageToShare} />
-                {isDownload ?
-                    <>
+            {isBgImgLoaded && <div className='mx-auto max-w-[400px]'>
+                <img src={imagePath} alt="result-img" className='mx-auto' ref={imageToShare} onLoad={() => setImageLoaded(true)} />
+                {imageLoaded && <>
+                    {isDownload ?
                         <div className=' text-white'>
                             <p className='py-3 text-center'>長按圖片進行下載</p>
                             <p>分享到社群邀請朋友測驗，尋找你的冒險夥伴！</p>
@@ -109,44 +120,44 @@ export default function Result() {
                                 </button>
                             </div>
                             {/* <div className='flex space-x-2 py-4'>
-                                <a href='https://www.instagram.com/create/story' className='flex items-center justify-center border rounded-lg py-1.5 w-1/2' target='blank'>
-                                    <img src={getImageUrl('result-element', 'ig')} alt="instagram" className='w-8 h-8' />
-                                    <span className='mx-2'>Instagram</span>
-                                </a>
-
-                                <div className="fb-share-button border rounded-lg w-1/2 py-1.5" data-href={resultPageUri} data-layout="" data-size="">
-                                    <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${resultPageUri}`} className="fb-xfbml-parse-ignore flex items-center justify-center">
-                                        <img src={getImageUrl('result-element', 'fb')} alt="facebook" className='w-8 h-8' />
-                                        <span className='mx-2'>Facebook</span>
-                                    </a>
-                                </div>
-                            </div> */}
-                        </div>
-                    </> :
-                    <>
+                                                    <a href='https://www.instagram.com/create/story' className='flex items-center justify-center border rounded-lg py-1.5 w-1/2' target='blank'>
+                                                        <img src={getImageUrl('result-element', 'ig')} alt="instagram" className='w-8 h-8' />
+                                                        <span className='mx-2'>Instagram</span>
+                                                    </a>
+                    
+                                                    <div className="fb-share-button border rounded-lg w-1/2 py-1.5" data-href={resultPageUri} data-layout="" data-size="">
+                                                        <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${resultPageUri}`} className="fb-xfbml-parse-ignore flex items-center justify-center">
+                                                            <img src={getImageUrl('result-element', 'fb')} alt="facebook" className='w-8 h-8' />
+                                                            <span className='mx-2'>Facebook</span>
+                                                        </a>
+                                                    </div>
+                                                </div> */}
+                        </div> :
                         <button className='rounded-full bg-white w-full py-4 mt-6' onClick={() => setIsDownload(true)}>取得結果圖</button>
-                    </>}
-                <img src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
-                <div className='text-center text-white'>
-                    <p className='py-1.5'>蓋亞資訊</p>
-                    <p>雲端服務整合專家</p>
-                    <p className='py-4 text-xl'>GAIA INFORMATION TECHNOLOGY</p>
-                    <img src={getImageUrl('result-element', 'gaia_logo_white', 'svg')} alt="gaia-logo" className='w-48 text-center inline-block pb-4' />
-                    <div className='text-xl'>
-                        <p>提供企業一站式雲端整合顧問服務</p>
-                        <p>24hr 中英雙語線上維運服務，</p>
-                        <p>快速解決客戶問題及需求。</p>
+                    }
+                    <img src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
+                    <div className='text-center text-white'>
+                        <p className='py-1.5'>蓋亞資訊</p>
+                        <p>雲端服務整合專家</p>
+                        <p className='py-4 text-xl'>GAIA INFORMATION TECHNOLOGY</p>
+                        <img src={getImageUrl('result-element', 'gaia_logo_white', 'svg')} alt="gaia-logo" className='w-48 text-center inline-block pb-4' />
+                        <div className='text-xl'>
+                            <p>提供企業一站式雲端整合顧問服務</p>
+                            <p>24hr 中英雙語線上維運服務，</p>
+                            <p>快速解決客戶問題及需求。</p>
+                        </div>
                     </div>
-                </div>
-                <a href="https://www.gaia.net/tc" target='blank' className='block text-center rounded-full bg-white w-full py-4 mt-6'>立即登入GAIA 新世界</a>
-                <img src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
-                <div className='text-center text-white text-xl'>
-                    <p className=''>攜手 GAIA</p>
-                    <p>打造未來雲端趨勢</p>
-                </div>
-                <img src={getImageUrl('result-element', 'clouds')} alt="clouds" className='my-16' />
-                <a href="https://www.gaia.net/tc/services/2/cloudcomputing" target='blank' className='block text-center rounded-full bg-white w-full py-4 mt-6'>了解更多上雲資訊</a>
-            </div>
+                    <a href="https://www.gaia.net/tc" target='blank' className='block text-center rounded-full bg-white w-full py-4 mt-6'>立即登入GAIA 新世界</a>
+                    <img src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
+                    <div className='text-center text-white text-xl'>
+                        <p className=''>攜手 GAIA</p>
+                        <p>打造未來雲端趨勢</p>
+                    </div>
+                    <img src={getImageUrl('result-element', 'clouds')} alt="clouds" className='my-16' />
+                    <a href="https://www.gaia.net/tc/services/2/cloudcomputing" target='blank' className='block text-center rounded-full bg-white w-full py-4 mt-6'>了解更多上雲資訊</a>
+                </>
+                }
+            </div>}
         </div >
     )
 }
