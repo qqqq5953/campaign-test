2bc7f2458a44e571c9b717589ea863e989e745f2
AndyHsieh <andy.hsieh@gaiatechs.com>
2023-09-06 17:08:51 +0800

    remove touchmove test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,9 +30,9 @@ export default function Home() {
         }
     }, [])
 
-    document.body.addEventListener('touchmove', function (e) {
-        e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-    }, { passive: false });
+    // document.body.addEventListener('touchmove', function (e) {
+    //     e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+    // }, { passive: false });
 
     return (
         <Layout>
