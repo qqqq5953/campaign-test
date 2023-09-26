f8ff9cf6ae6e59189c3475713e339b74b2a80fa6
AndyHsieh <andy.hsieh@gaiatechs.com>
2023-09-26 15:13:31 +0800

    add lazy loading

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -88,12 +88,12 @@ export default function Result() {
                         <p className='text-center'>分享到社群邀請朋友測驗，尋找你的冒險夥伴！</p>
                         {isMobile ? <MobileBtn imageToShare={imageToShare} /> : <WebBtn />}
                     </div>
-                    <img src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
+                    <img loading="lazy" src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
                     <div className='text-center text-white'>
                         <p className='py-1.5'>蓋亞資訊</p>
                         <p>雲端服務整合專家</p>
                         <p className='py-4 text-xl'>GAIA INFORMATION TECHNOLOGY</p>
-                        <img src={getImageUrl('result-element', 'gaia_logo_white', 'svg')} alt="gaia-logo" className='w-48 text-center inline-block pb-4' />
+                        <img loading="lazy" src={getImageUrl('result-element', 'gaia_logo_white', 'svg')} alt="gaia-logo" className='w-48 text-center inline-block pb-4' />
                         <div className='text-xl'>
                             <p>提供企業一站式雲端整合顧問服務</p>
                             <p>24hr 中英雙語線上維運服務，</p>
@@ -101,12 +101,12 @@ export default function Result() {
                         </div>
                     </div>
                     <a href="https://www.gaia.net/tc" target='blank' className='block text-center rounded-full bg-white w-full py-4 mt-6'>立即登入GAIA 新世界</a>
-                    <img src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
+                    <img loading="lazy" src={getImageUrl('result-element', 'divide')} alt="divide" className='my-16' />
                     <div className='text-center text-white text-xl'>
                         <p className=''>攜手 GAIA</p>
                         <p>打造未來雲端趨勢</p>
                     </div>
-                    <img src={getImageUrl('result-element', 'clouds')} alt="clouds" className='my-16' />
+                    <img loading="lazy" src={getImageUrl('result-element', 'clouds')} alt="clouds" className='my-16' />
                     <a href="https://www.gaia.net/tc/services/2/cloudcomputing" target='blank' className='block text-center rounded-full bg-white w-full py-4 mt-6'>了解更多上雲資訊</a>
                 </>
                 }
@@ -150,13 +150,13 @@ function WebBtn() {
 
     return <div className='flex space-x-2 py-4'>
         <a href='https://www.instagram.com/create/story' className='flex items-center justify-center border rounded-lg py-1.5 w-1/2' target='blank'>
-            <img src={getImageUrl('result-element', 'ig')} alt="instagram" className='w-8 h-8' />
+            <img loading="eager" src={getImageUrl('result-element', 'ig')} alt="instagram" className='w-8 h-8' />
             <span className='mx-2'>Instagram</span>
         </a>
 
         <div className="fb-share-button border rounded-lg w-1/2 py-1.5" data-href={fbShareLink} data-layout="" data-size="">
             <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${fbShareLink}`} className="fb-xfbml-parse-ignore flex items-center justify-center">
-                <img src={getImageUrl('result-element', 'fb')} alt="facebook" className='w-8 h-8' />
+                <img loading="eager" src={getImageUrl('result-element', 'fb')} alt="facebook" className='w-8 h-8' />
                 <span className='mx-2'>Facebook</span>
             </a>
         </div>
@@ -164,7 +164,7 @@ function WebBtn() {
 }
 
 function getRoleImage(score) {
-    let imageName = ''
+    let imageName = "knight"
 
     if (score >= 0 && score <= 5) {
         imageName = "knight"
